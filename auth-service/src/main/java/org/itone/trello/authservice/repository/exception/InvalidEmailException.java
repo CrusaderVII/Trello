f6525efe5768c0fc5640d82bd650bd5de6b12242
CrusaderVII <egor.chervonikov@yandex.ru>
2024-04-24 00:40:00 +0300

    Deleted auth service.

--- auth-service/src/main/java/org/itone/trello/authservice/repository/exception/InvalidEmailException.java
+++
@@ -1,8 +0,0 @@
-package org.itone.trello.authservice.repository.exception;
-
-public class InvalidEmailException extends RuntimeException{
-    private static final String PREFIX = "Invalid email";
-    public InvalidEmailException () {
-        super(PREFIX);
-    }
-}
--- auth-service/src/main/java/org/itone/trello/authservice/repository/exception/NoSuchUserException.java
+++
@@ -1,9 +0,0 @@
-package org.itone.trello.authservice.repository.exception;
-
-public class NoSuchUserException extends RuntimeException{
-    private static final String PREFIX = "There is no user with name %s";
-
-    public NoSuchUserException (String login) {
-        super(String.format(PREFIX, login));
-    }
-}
